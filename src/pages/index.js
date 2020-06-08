ec01e1b385ed226543fbf66c1dda6dc13296571e
maximalyono <maximalyono@gmail.com>
2020-06-08 16:26:50 +0700

    mengubah palembang jadi meranjat di index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi Wong PALEMBANG galo-galonyo</h1>
+    <h1>Hi Wong Meranjat galo-galonyo</h1>
     <p>Welcome to your new Gatsby site, palembang digital.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
